914eedafbfb6e78834b4423566819856b61eb781
lzhangwei <zhangwei_999@foxmail.com>
2014-10-09 23:23:31 +0800

    add get set and construct in Cart calss

--- src/main/java/com/thoughtworks/iamcoach/pos/Cart.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Cart.java
@@ -1,7 +1,22 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.util.LinkedList;
+
 /**
  * Created by zhangwei on 14-10-9.
  */
 public class Cart {
+    private LinkedList<CartItem> cartItemList;
+
+    public LinkedList<CartItem> getCartItemList() {
+        return cartItemList;
+    }
+
+    public void setCartItemList(LinkedList<CartItem> cartItemList) {
+        this.cartItemList = cartItemList;
+    }
+
+    public Cart() {
+        this.cartItemList = new LinkedList<CartItem>();
+    }
 }
