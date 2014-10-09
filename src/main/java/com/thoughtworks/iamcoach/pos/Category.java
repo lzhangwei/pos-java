586a444163a04818093739820a812cc094aa2e1a
lzhangwei <zhangwei_999@foxmail.com>
2014-10-09 23:13:13 +0800

    add get set and construct in Category calss

--- src/main/java/com/thoughtworks/iamcoach/pos/Category.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Category.java
@@ -4,4 +4,27 @@ package com.thoughtworks.iamcoach.pos;
  * Created by zhangwei on 14-10-9.
  */
 public class Category {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
