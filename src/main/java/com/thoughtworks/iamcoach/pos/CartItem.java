a66c26ed30218dc2527db147c35893f9e9af5a27
lzhangwei <zhangwei_999@foxmail.com>
2014-10-09 23:21:33 +0800

    add get set and construct in CartItem calss

--- src/main/java/com/thoughtworks/iamcoach/pos/CartItem.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/CartItem.java
@@ -4,4 +4,27 @@ package com.thoughtworks.iamcoach.pos;
  * Created by zhangwei on 14-10-9.
  */
 public class CartItem {
+    private Item item;
+    private int num;
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public CartItem(Item item, int num) {
+        this.item = item;
+        this.num = num;
+    }
 }
