0e52707838501904f5edb0fd8ca6d0c48aa249da
lzhangwei <zhangwei_999@foxmail.com>
2014-10-10 23:33:24 +0800

    add addItem function in Cart

--- src/main/java/com/thoughtworks/iamcoach/pos/Cart.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Cart.java
@@ -16,4 +16,19 @@ public class Cart {
     public Cart() {
         this.cartItemList = new LinkedList<CartItem>();
     }
+
+    public void addItem(Item item) {
+        int tag = 0;
+        for (int i = 0; i < cartItemList.size(); i++) {
+            if (cartItemList.get(i).getItem().getId() == item.getId()) {
+                int num = cartItemList.get(i).getNum();
+                cartItemList.get(i).setNum(num + 1);
+                tag = 1;
+                break;
+            }
+        }
+        if (tag == 0) {
+            cartItemList.push(new CartItem(item, 1));
+        }
+    }
 }
