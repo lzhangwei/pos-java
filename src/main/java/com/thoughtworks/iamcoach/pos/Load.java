08ce0b53cca514a8cf95f0944a70ec37c39c79fc
lzhangwei <zhangwei_999@foxmail.com>
2014-10-11 20:20:48 +0800

    add filed and get and set

--- src/main/java/com/thoughtworks/iamcoach/pos/Load.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Load.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.util.ArrayList;
+
 public class Load {
+    private ArrayList<Item> items;
+    private ArrayList<Category> categories;
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public ArrayList<Category> getCategories() {
+        return categories;
+    }
+
 }
