09d73bc8d424215716894c3da1f0b58c8ee29de7
lzhangwei <zhangwei_999@foxmail.com>
2014-10-11 20:16:37 +0800

    add Load class

--- src/main/java/com/thoughtworks/iamcoach/pos/Scanner.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Scanner.java
@@ -1,4 +1,25 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Scanner {
+
+    public LinkedList<CartItem> readCartFile(Path path) {
+        LinkedList<CartItem> result = new LinkedList<CartItem>();
+        try {
+            List<String> fileLines = Files.readAllLines(path);
+            for (int i = 0; i < fileLines.size(); i++) {
+                String[] barcode = fileLines.get(i).split("-");
+
+            }
+        } catch (Exception e) {
+            System.out.print("throw IOException when read all lines!");
+        }
+
+        return result;
+    }
+
 }
