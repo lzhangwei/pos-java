e7cf3c1658de99c08e750d10c01cc74c6554d9e6
lzhangwei <zhangwei_999@foxmail.com>
2014-10-12 17:50:04 +0800

    delete parameter

--- src/main/java/com/thoughtworks/iamcoach/pos/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Promotion.java
@@ -2,12 +2,14 @@ package com.thoughtworks.iamcoach.pos;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Promotion {
 
-    public List<String> readBuyTwoGetOneFile(Path path) {
+    public List<String> readBuyTwoGetOneFile() {
+        Path path = Paths.get("../resources/buy_two_get_one_free_promotion.txt");
         List<String> result = new ArrayList<String>();
         try {
             List<String> fileLines = Files.readAllLines(path);
@@ -20,7 +22,8 @@ public class Promotion {
         return result;
     }
 
-    public List<String> readSecondHalfPriceFile(Path path) {
+    public List<String> readSecondHalfPriceFile() {
+        Path path = Paths.get("../resources/second_half_price_promotion.txt");
         List<String> result = new ArrayList<String>();
         try {
             List<String> fileLines = Files.readAllLines(path);
