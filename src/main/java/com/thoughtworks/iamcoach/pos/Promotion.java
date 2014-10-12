58381e86b07633cb5bb81d592a114cb47ca502bb
lzhangwei <zhangwei_999@foxmail.com>
2014-10-12 17:37:03 +0800

    add buy two get one function

--- src/main/java/com/thoughtworks/iamcoach/pos/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Promotion.java
@@ -1,4 +1,31 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Promotion {
+
+    private Load load;
+
+    {
+        this.load = new Load();
+    }
+
+    public List<Item> readBuyTwoGetOneFile(Path path) {
+        List<Item> result = new ArrayList<Item>();
+        try {
+            List<String> fileLines = Files.readAllLines(path);
+            for (int i = 0; i < fileLines.size(); i++) {
+                String barcode = fileLines.get(i);
+                result.set(i, load.findItemByBarcode(barcode));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    
 }
