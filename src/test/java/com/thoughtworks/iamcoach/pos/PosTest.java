e58e2b3bafc847e0f7793e63d6e11989f9a86b4c
lzhangwei <zhangwei_999@foxmail.com>
2014-10-20 13:13:56 +0800

    add parse varcode test

--- src/test/java/com/thoughtworks/iamcoach/pos/PosTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PosTest.java
@@ -2,12 +2,30 @@ package com.thoughtworks.iamcoach.pos;
 
 import org.junit.Test;
 
-import static org.fest.assertions.api.Assertions.assertThat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PosTest {
+
     @Test
-    public void hello_world_test() {
-        assertThat("Hello World").isEqualTo("Hello World");
+    public void parse_barcode_test() {
+        List<String> barcodes = new ArrayList<String>();
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000003-2");
+        barcodes.add("ITEM000005");
+        barcodes.add("ITEM000005");
+        barcodes.add("ITEM000005");
+        ArrayList<CartItem> cartItems = new ArrayList<CartItem>();
+        cartItems.add(new CartItem(new Item(1, "ITEM000001", "雪碧","瓶",3.00),5));
+        cartItems.add(new CartItem(new Item(3, "ITEM000003", "荔枝","千克",15.00),2));
+        cartItems.add(new CartItem(new Item(5, "ITEM000005", "方便面","袋",2.50),3));
+        Pos pos = new Pos();
+        pos.parseBarcode(barcodes);
+        assertThat(pos.getCartItems.size()).isEqualTo(3);
     }
     
 }
