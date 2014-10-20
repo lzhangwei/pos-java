5c9f205360f4344d842b21d9294de589b3c8907f
lzhangwei <zhangwei_999@foxmail.com>
2014-10-20 15:13:39 +0800

    delete final string

--- src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pos {
-    public static final String CART_FILE = "../resources/cart.txt";
-    public static final String FREE_FILE = "../resources/buy_two_get_one_free_promotion.txt";
-    public static final String HALF_FILE = "../resources/second_half_price_promotion.txt";
-    public static final String DISCOUNT_FILE = "../resources/discount_promotion.txt";
 
     private ArrayList<Item> items;
     private ArrayList<CartItem> cartItems;
