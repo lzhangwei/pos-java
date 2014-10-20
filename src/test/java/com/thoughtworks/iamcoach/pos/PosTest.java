b2081a4f699859b522a99df5e2a4a3fe840d854f
lzhangwei <zhangwei_999@foxmail.com>
2014-10-20 13:34:48 +0800

    add discount promotion test

--- src/test/java/com/thoughtworks/iamcoach/pos/PosTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PosTest.java
@@ -75,4 +75,28 @@ public class PosTest {
         assertThat(pos.getCartItems.get(1).getSumPrice()).isEqualTo(15);
         assertThat(pos.getCartItems.get(1).getPromotionPrice()).isEqualTo(3);
     }
+
+    @Test
+    public void calculate_discount_promotion_test() {
+        List<String> barcodes = new ArrayList<String>();
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000001");
+        barcodes.add("ITEM000003-2");
+        barcodes.add("ITEM000005");
+        barcodes.add("ITEM000005");
+        barcodes.add("ITEM000005");
+        List<String> freeBarcodes = new ArrayList<String>();
+        freeBarcodes.add("ITEM000001:75");
+        freeBarcodes.add("ITEM000003:85");
+        freeBarcodes.add("ITEM000005:90");
+        Pos pos = new Pos();
+        pos.parseBarcode(barcodes);
+        pos.calDiscountPromotion(freeBarcodes);
+        assertThat(pos.getCartItems.size()).isEqualTo(3);
+        assertThat(pos.getCartItems.get(1).getSumPrice()).isEqualTo(15);
+        assertThat(pos.getCartItems.get(1).getPromotionPrice()).isEqualTo(3.75);
+    }
 }
