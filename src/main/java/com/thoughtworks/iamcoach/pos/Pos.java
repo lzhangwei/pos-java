efccea81ed2405554192313b9ed40f1c3b4b2b49
lzhangwei <zhangwei_999@foxmail.com>
2014-10-20 14:20:14 +0800

    add final string

--- src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Pos.java
@@ -3,6 +3,11 @@ package com.thoughtworks.iamcoach.pos;
 import java.util.ArrayList;
 
 public class Pos {
+    public static final String CART_FILE = "../resources/cart.txt";
+    public static final String FREE_FILE = "../resources/buy_two_get_one_free_promotion.txt";
+    public static final String HALF_FILE = "../resources/second_half_price_promotion.txt";
+    public static final String DISCOUNT_FILE = "../resources/discount_promotion.txt";
+
     private ArrayList<Item> items;
     private ArrayList<CartItem> cartItems;
     private double sumPrice;
@@ -15,5 +20,6 @@ public class Pos {
         this.items.set(4, new Item(4, "ITEM000004", "电池", "瓶", 3.00));
         this.items.set(5, new Item(5, "ITEM000005", "方便面", "瓶", 3.00));
     }
-    
+
+
 }
