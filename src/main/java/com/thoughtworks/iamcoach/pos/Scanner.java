d4a808a27e182905caf47b5f44160b68b69d1b0f
lzhangwei <zhangwei_999@foxmail.com>
2014-10-20 14:13:57 +0800

    add readFile function

--- src/main/java/com/thoughtworks/iamcoach/pos/Scanner.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Scanner.java
@@ -8,22 +8,11 @@ import java.util.List;
 
 public class Scanner {
 
-    private Load load;
-
-    {
-        this.load = new Load();
-    }
-
-    public LinkedList<CartItem> readCartFile() {
-        Path path = Paths.get("../resources/cart.txt");
-        LinkedList<CartItem> result = new LinkedList<CartItem>();
+    public List<String> readFile(String pathstr) {
+        Path path = Paths.get(pathstr);
+        List<String> result = null;
         try {
-            List<String> fileLines = Files.readAllLines(path);
-            for (int i = 0; i < fileLines.size(); i++) {
-                String[] barcode = fileLines.get(i).split("-");
-                CartItem cartItem = new CartItem(load.findItemByBarcode(barcode[0]),Integer.parseInt(barcode[1]));
-                result.push(cartItem);
-            }
+            result = Files.readAllLines(path);
         } catch (Exception e) {
             e.printStackTrace();
         }
