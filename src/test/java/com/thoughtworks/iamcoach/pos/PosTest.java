3456084ab5337e518ea0b9ce89db433292b2a753
lzhangwei <zhangwei_999@foxmail.com>
2014-10-20 12:54:05 +0800

    add pos test file

---
+++ src/test/java/com/thoughtworks/iamcoach/pos/PosTest.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.iamcoach.pos;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class PosTest {
+    @Test
+    public void hello_world_test() {
+        assertThat("Hello World").isEqualTo("Hello World");
+    }
+    
+}
