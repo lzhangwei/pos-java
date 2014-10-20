0f6053e5b820bd54d9cdbbd66035f406beb2c2e9
lzhangwei <zhangwei_999@foxmail.com>
2014-10-20 15:24:34 +0800

    update main function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,9 @@
+import com.thoughtworks.iamcoach.pos.CartItem;
+import com.thoughtworks.iamcoach.pos.Pos;
+import com.thoughtworks.iamcoach.pos.Scanner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
@@ -7,6 +13,27 @@ public class App {
     public static final String DISCOUNT_FILE = "../resources/discount_promotion.txt";
 
     public static void main(String[] args) {
-        //TODO: Need to implement.
+        Scanner scanner = new Scanner();
+        List<String> cartItemBarcodes = scanner.readFile(CART_FILE);
+
+        Pos pos = new Pos();
+        pos.parseBarcode(cartItemBarcodes);
+
+        List<String> freeItemBarcodes = scanner.readFile(FREE_FILE);
+        ArrayList<CartItem> freeCartItems = pos.calFreePromotion(freeItemBarcodes);
+
+        List<String> halfItemBarcodes = scanner.readFile(HALF_FILE);
+        ArrayList<CartItem> halfCartItems = pos.calHalfPromotion(halfItemBarcodes);
+
+        List<String> discountItemBarcodes = scanner.readFile(DISCOUNT_FILE);
+        ArrayList<CartItem> discountCartItems = pos.calDiscountPromotion(discountItemBarcodes);
+
+        pos.comparePrice(freeCartItems,halfCartItems,discountCartItems);
+
+        print(pos);
+    }
+
+    private static void print(Pos pos) {
+        
     }
 }
