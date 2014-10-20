3a185dd42af93fef3c39cdf5c738b5405922e533
lzhangwei <zhangwei_999@foxmail.com>
2014-10-20 15:14:37 +0800

    add final file name

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,12 @@
+
 public class App {
+
+    public static final String CART_FILE = "../resources/cart.txt";
+    public static final String FREE_FILE = "../resources/buy_two_get_one_free_promotion.txt";
+    public static final String HALF_FILE = "../resources/second_half_price_promotion.txt";
+    public static final String DISCOUNT_FILE = "../resources/discount_promotion.txt";
+
     public static void main(String[] args) {
         //TODO: Need to implement.
-
     }
 }
